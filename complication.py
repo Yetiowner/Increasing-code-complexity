82b9d52f8e394243adb4523e54322cb7dc27762f
Corsin <74185591+C0RR1T@users.noreply.github.com>
2022-10-19 21:23:27 +0200

    New complexity

--- complication.py
+++ complication.py
@@ -1,4 +1,5 @@
-HW = "Hello World!"
-for i in (HW.replace(" ", "").rstrip("!")):
-    print(HW)
+HW = "Hello World!\n"
+for i in (HW.replace(" ", "").rstrip("!\n")):
+    for y in HW:
+        print(y, end="")
 
