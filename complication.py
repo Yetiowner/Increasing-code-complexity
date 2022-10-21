e16c84b14f9f17374302c27724f75256665ae89c
Oliver Gibson <ojrgibson@perse.co.uk>
2022-10-21 19:58:03 +0100

    Day 5

--- complication.py
+++ complication.py
@@ -1,5 +1,14 @@
 import helloworld
-HW = "Hello World!"
+import helloworld_builder
+
+
+helloworld_chars = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!']
+
+builder = helloworld_builder.HelloWorldBuilder()
+for c in helloworld_chars:
+    builder.add_char(c)
+
+HW = builder.build()
 for i in (HW.replace(" ", "").rstrip("!")):
     helloworld.c_helloworld(HW)
 
