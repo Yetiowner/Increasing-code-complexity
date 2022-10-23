650ca52fff8cba8c39f2933892d3045380cc52f8
Oliver Gibson <ojrgibson@perse.co.uk>
2022-10-23 20:20:48 +0100

    Day 7

--- README.md
+++ README.md
@@ -26,6 +26,10 @@ Day 6:
 Converted string builder class to singleton factory
 See https://www.reddit.com/r/ProgrammerHumor/comments/yavooh/see_comments_for_github_repo_that_includes_all/
 
-Make sure to go to https://www.reddit.com/r/ProgrammerHumor/comments/yavooh/see_comments_for_github_repo_that_includes_all/ to vote on the next change!
+Day 7:
+Created REST API for getting characters out of binary strings
+See https://www.reddit.com/r/ProgrammerHumor/comments/ybqcsk/see_comments_for_github_repo_that_includes_all/
+
+Make sure to go to https://www.reddit.com/r/ProgrammerHumor/comments/ybqcsk/see_comments_for_github_repo_that_includes_all/ to vote on the next change!
 
 DO NOT VOTE FOR CHANGES USING ISSUES OR PULL REQUESTS (Except for the rust and unit test pull request, you can stay)
--- complication.py
+++ complication.py
@@ -1,8 +1,16 @@
 import helloworld
 import helloworld_builder
+import subprocess
+import requests
 
+# Before running, run the command "pythonw.exe restapi.pyw 1>NUL"
 
-helloworld_chars = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!']
+binvals = ['1001000', '1100101', '1101100', '1101100', '1101111', '100000', '1010111', '1101111', '1110010', '1101100', '1100100', '100001']
+helloworld_chars = []
+for val in binvals:
+    response = requests.get(f"http://127.0.0.1:5002/characters/{val}")
+    helloworld_chars.append(str(response.content).lstrip("b'\"").rstrip("\"\\n\'"))
+
 
 builder = helloworld_builder.HelloWorldBuilder.instance()
 for c in helloworld_chars:
@@ -11,5 +19,8 @@ for c in helloworld_chars:
 HW = builder.build()
 for i in (HW.replace(" ", "").rstrip("!")):
     helloworld.c_helloworld(HW)
+
+subprocess.run(['taskkill', '/IM', 'pythonw.exe', '/F'], capture_output=True)
+
 
     
